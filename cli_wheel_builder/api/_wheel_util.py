a0a3882a69454fc3e14d8a9f3f00481431f60739
Timo Reymann <timo.reymann@deepl.com>
2023-12-22 17:28:18 +0100

    fix: Attempt to purge all m$ specific metadata

--- cli_wheel_builder/api/_wheel_util.py
+++ cli_wheel_builder/api/_wheel_util.py
@@ -29,13 +29,14 @@ class ReproducibleWheelFile(WheelFile):
         zip_info = ZipInfo(wheel_entry.path)
         zip_info.external_attr = (wheel_entry.permissions | stat.S_IFREG) << 16
         zip_info.file_size = len(wheel_entry.content)
+        zip_info.create_system = 3
         self.writestr(zip_info, data=wheel_entry.content)
 
     def writestr(self, zip_info: ZipInfo | str, *args, **kwargs):
         if isinstance(zip_info, str):
             zip_info = ZipInfo(zip_info)
             zip_info.external_attr = (0o644 | stat.S_IFREG) << 16
-        zip_info.compress_type = self.compression
+        zip_info.compress_type = ZIP_DEFLATED
         zip_info.date_time = (1980, 1, 1, 0, 0, 0)
         zip_info.create_system = 3
         super().writestr(zip_info, *args, **kwargs)

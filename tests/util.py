ead92412f1ca3d7269e6efad3c3eac253b691800
Timo Reymann <timo.reymann@deepl.com>
2023-12-22 16:48:50 +0100

    ci: Add exe suffix for executable on windows

--- tests/util.py
+++ tests/util.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import subprocess
 from pathlib import Path
 
@@ -8,8 +9,8 @@ def install_wheel(path: Path | str, name : str):
 
 
 def verify_install(package: str, args: str = "--version"):
-    output = subprocess.check_output(f"{package} {args}", shell=True, text=True)
+    output = subprocess.check_output(f"{package}{'.exe' if platform.system() == 'Windows' else ''} {args}", shell=True, text=True)
     assert output != ""
 
-    output = subprocess.check_output(f"python3 -m {package.replace('-','_')} {args}", shell=True, text=True)
+    output = subprocess.check_output(f"python{'.exe' if platform.system() == 'Windows' else ''} -m {package.replace('-','_')} {args}", shell=True, text=True)
     assert output != ""

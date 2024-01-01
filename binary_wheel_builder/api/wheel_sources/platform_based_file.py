45bf55baf16c86712ee24f51472eabcbdcf7a131
Timo Reymann <timo.reymann@deepl.com>
2024-01-01 22:36:38 +0100

    feat: Add platform based wheel source

---
+++ binary_wheel_builder/api/wheel_sources/platform_based_file.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from binary_wheel_builder.api.meta import WheelSource, WheelPlatformIdentifier, WheelFileEntry
+from binary_wheel_builder.api.wheel_sources.exceptions import UnsupportedWheelPlatformException, SourceFileRequestFailed
+
+
+class PlatformBasedFileSource(WheelSource):
+    def __init__(self, file_name_mapping: dict[WheelPlatformIdentifier, Path]):
+        self.file_name_mapping = file_name_mapping
+
+    def generate_fileset(self, wheel_platform: WheelPlatformIdentifier) -> list[WheelFileEntry]:
+        if wheel_platform not in self.file_name_mapping:
+            raise UnsupportedWheelPlatformException(wheel_platform)
+
+        file = self.file_name_mapping[wheel_platform]
+
+        if not file.exists() or not file.is_file():
+            raise SourceFileRequestFailed(f"File {file} does not resolve to a valid file")
+
+        return [
+            WheelFileEntry(
+                path=file.name,
+                content=file.read_bytes(),
+                permissions=0o755
+            )
+        ]
---
+++ binary_wheel_builder/api/wheel_sources/platform_based_file_test.py
@@ -0,0 +1,29 @@
+import tempfile
+from pathlib import Path
+
+import pytest
+
+from binary_wheel_builder.api import well_known_platforms
+from binary_wheel_builder.api.wheel_sources.exceptions import SourceFileRequestFailed
+from binary_wheel_builder.api.wheel_sources.platform_based_file import PlatformBasedFileSource
+
+
+def test_platform_based_file_invalid():
+    source = PlatformBasedFileSource({
+        well_known_platforms.LINUX_GENERIC_i386: Path("linux-x86"),
+    })
+
+    with pytest.raises(SourceFileRequestFailed):
+        source.generate_fileset(well_known_platforms.LINUX_GENERIC_i386)
+
+
+def test_platform_based_file_valid():
+    with tempfile.NamedTemporaryFile("w") as f:
+        f.write("test")
+        f.flush()
+
+        source = PlatformBasedFileSource({
+            well_known_platforms.LINUX_GENERIC_i386: Path(f.name),
+        })
+
+        source.generate_fileset(well_known_platforms.LINUX_GENERIC_i386)

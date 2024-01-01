5952378964e9993390fc8492f800ae34d54c8f6e
Timo Reymann <timo.reymann@deepl.com>
2024-01-01 22:47:11 +0100

    fix: Add API import for platform based file source

--- binary_wheel_builder/api/wheel_sources/__init__.py
+++ binary_wheel_builder/api/wheel_sources/__init__.py
@@ -1,3 +1,4 @@
 from binary_wheel_builder.api.wheel_sources.github import GithubReleaseBinarySource
 from binary_wheel_builder.api.wheel_sources.static import StaticLocalWheelSource
+from binary_wheel_builder.api.wheel_sources.platform_based_file import PlatformBasedFileSource
 import binary_wheel_builder.api.wheel_sources.exceptions

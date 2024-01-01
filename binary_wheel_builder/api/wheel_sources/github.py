d32403162fdc13fe479f1d77faf5daeafe5e2bcc
Timo Reymann <timo.reymann@deepl.com>
2024-01-01 22:36:25 +0100

    feat: Harden github release source

--- binary_wheel_builder/api/wheel_sources/github.py
+++ binary_wheel_builder/api/wheel_sources/github.py
@@ -1,4 +1,7 @@
+from urllib.error import HTTPError
+
 from binary_wheel_builder.api.meta import WheelSource, WheelPlatformIdentifier, WheelFileEntry
+from binary_wheel_builder.api.wheel_sources.exceptions import UnsupportedWheelPlatformException, SourceFileRequestFailed
 
 
 class GithubReleaseBinarySource(WheelSource):
@@ -18,14 +21,16 @@ class GithubReleaseBinarySource(WheelSource):
         from urllib.request import urlopen
 
         if wheel_platform not in self.asset_name_mapping:
-            print(self.asset_name_mapping)
-            raise Exception(wheel_platform)
+            raise UnsupportedWheelPlatformException(wheel_platform)
 
         url = (f"https://github.com/{self.project_slug}"
                f"/releases/download/{self.tag_prefix}{self.version}/{self.asset_name_mapping[wheel_platform]}")
 
-        with urlopen(url) as response:
-            file_content = response.read()
+        try:
+            with urlopen(url) as response:
+                file_content = response.read()
+        except HTTPError as e:
+            raise SourceFileRequestFailed("Failed to fetch file: " + str(e)) from e
 
         return [
             WheelFileEntry(
---
+++ binary_wheel_builder/api/wheel_sources/github_test.py
@@ -0,0 +1,42 @@
+import urllib.request
+from unittest import mock
+from unittest.mock import patch, MagicMock
+from urllib.error import HTTPError
+
+import pytest
+
+from binary_wheel_builder.api import well_known_platforms
+from binary_wheel_builder.api.wheel_sources import GithubReleaseBinarySource
+from binary_wheel_builder.api.wheel_sources.exceptions import WheelSourceException
+
+
+@pytest.mark.parametrize([
+    "tag_prefix",
+    "status_code",
+], [
+    ["v", 404],
+    ["", 404]
+])
+def test_github_release_binary_wheel_source(tag_prefix: str, status_code: int):
+    request_fail = True if status_code < 200 or status_code > 300 else False
+    with mock.patch("urllib.request.urlopen",
+                    return_value=MagicMock(return_value=None),
+                    side_effect=HTTPError('http://example.com', 500, 'Internal Error', {},
+                                          None) if request_fail else None) as urlopen_mock:
+        source = GithubReleaseBinarySource(
+            "org/project",
+            "0.0.1", {
+                well_known_platforms.LINUX_GENERIC_i386: "foo-bar"
+            },
+            "foo-bar",
+            tag_prefix
+        )
+
+        if request_fail:
+            with pytest.raises(WheelSourceException):
+                source.generate_fileset(well_known_platforms.LINUX_GENERIC_i386)
+        else:
+            source.generate_fileset(well_known_platforms.LINUX_GENERIC_i386)
+            urlopen_mock.read.assert_called_once()
+
+        urlopen_mock.assert_called_once()

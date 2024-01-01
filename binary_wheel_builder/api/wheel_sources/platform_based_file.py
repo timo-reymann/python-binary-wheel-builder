bb0fed7421dac68a98df9199e135968cd1745721
Timo Reymann <timo.reymann@deepl.com>
2024-01-01 22:48:32 +0100

    fix: Allow string mapping to make platform based resolver work with yaml files

--- binary_wheel_builder/api/wheel_sources/platform_based_file.py
+++ binary_wheel_builder/api/wheel_sources/platform_based_file.py
@@ -5,7 +5,7 @@ from binary_wheel_builder.api.wheel_sources.exceptions import UnsupportedWheelPl
 
 
 class PlatformBasedFileSource(WheelSource):
-    def __init__(self, file_name_mapping: dict[WheelPlatformIdentifier, Path]):
+    def __init__(self, file_name_mapping: dict[WheelPlatformIdentifier, Path | str]):
         self.file_name_mapping = file_name_mapping
 
     def generate_fileset(self, wheel_platform: WheelPlatformIdentifier) -> list[WheelFileEntry]:
@@ -13,6 +13,9 @@ class PlatformBasedFileSource(WheelSource):
             raise UnsupportedWheelPlatformException(wheel_platform)
 
         file = self.file_name_mapping[wheel_platform]
+
+        if isinstance(file, str):
+            file = Path(file)
 
         if not file.exists() or not file.is_file():
             raise SourceFileRequestFailed(f"File {file} does not resolve to a valid file")

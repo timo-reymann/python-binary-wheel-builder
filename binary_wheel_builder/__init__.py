36a7af12eb10618bdf956e6e46ec0f56d752e44d
Timo Reymann <mail@timo-reymann.de>
2024-12-14 13:26:39 +0100

    feat: Add version attribute for binary_wheel_builder package

--- binary_wheel_builder/__init__.py
+++ binary_wheel_builder/__init__.py
@@ -1,4 +1,5 @@
 """
 For convenience of use this module re-exports everything from the .api package
 """
+from binary_wheel_builder.__version__ import __version__
 from binary_wheel_builder.api import *
